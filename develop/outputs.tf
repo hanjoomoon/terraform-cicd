21793635aecf2462a4f66b9ddbcb1786e2725771
hanjoomoon <dev-luis@kakao.com>
2022-11-14 22:38:54 +0900

    feat: outputs.tf 파일 추가 및 resources 정의 수정

--- develop/outputs.tf
+++ develop/outputs.tf
@@ -8,13 +8,3 @@ output "all_arns" {
   value       = aws_iam_user.myiam[*].arn
   description = "The ARNs for all users"
 }
-
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.mys3bucket.arn
-  description = "The ARN of the S3 bucket"
-}
-
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.mydynamodbtable.name
-  description = "The name of the DynamoDB table"
-}
